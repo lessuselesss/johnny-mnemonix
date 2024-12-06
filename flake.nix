b071ccc18e35c8d443578101f39dd0360552c144
Ashley Barr <lessuseless@duck.com>
2024-12-06 11:32:45 -0700

    update description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Johnny Mnemonix - Declaritive Document Manager";
+  description = "Johnny Mnemonix - A Johnny Decimal-based Declaritive Document Manager";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
