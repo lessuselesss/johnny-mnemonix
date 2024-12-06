6493de9dece85e6bcb805238b090d8edbca6d7c9
Ashley Barr <lessuseless@duck.com>
2024-12-06 00:29:43 -0700

    fix inherit

--- modules/johnny-mnemonix.nix
+++ modules/johnny-mnemonix.nix
@@ -1,6 +1,7 @@
-{ config
-, lib
-, ...
+{
+  config,
+  lib,
+  ...
 }:
 with lib; let
   cfg = config.johnny-mnemonix;
@@ -42,35 +43,59 @@ with lib; let
     };
   };
 
+  # New options for directory permissions
+  permissionsType = types.submodule {
+    options = {
+      dirMode = mkOption {
+        type = types.str;
+        default = "0755";
+        description = "Permissions for created directories (octal string)";
+        example = "0700";
+      };
+      user = mkOption {
+        type = types.str;
+        default = config.home.username;
+        description = "Owner of the directories";
+      };
+      group = mkOption {
+        type = types.str;
+        default = "users";
+        description = "Group owner of the directories";
+      };
+    };
+  };
+
   # Helper function to create directory paths
   mkDirPath = _: area: _: category: id: name: "${cfg.baseDir}/${area.name}/${category.name}/${id} ${name}";
 
   # Helper function to create directory entries for a single item
   mkItemDir = areaId: area: catId: category: id: name: {
-    "${mkDirPath areaId area catId category id name}".source = null;
+    "${mkDirPath areaId area catId category id name}" = {
+      source = null;
+      inherit (cfg.permissions) dirMode user group;
+    };
   };
 
   # Helper function to process all items in a category
   mkCategoryDirs = areaId: area: catId: category:
     mapAttrsToList
-      (id: name: mkItemDir areaId area catId category id name)
-      category.items;
+    (id: name: mkItemDir areaId area catId category id name)
+    category.items;
 
   # Helper function to process all categories in an area
   mkAreaDirs = areaId: area:
     concatLists
-      (mapAttrsToList
-        (catId: category: mkCategoryDirs areaId area catId category)
-        area.categories);
+    (mapAttrsToList
+      (catId: category: mkCategoryDirs areaId area catId category)
+      area.categories);
 
   # Create all directory entries
   mkDirs =
     foldAttrs
-      (x: _: x)
-      { }
-      (concatLists (mapAttrsToList mkAreaDirs cfg.areas));
-in
-{
+    (x: _: x)
+    {}
+    (concatLists (mapAttrsToList mkAreaDirs cfg.areas));
+in {
   options.johnny-mnemonix = {
     enable = mkEnableOption "Johnny Decimal document management";
 
@@ -82,14 +107,38 @@ in
 
     areas = mkOption {
       type = types.attrsOf areaType;
-      default = { };
+      default = {};
       description = "Map of area IDs to area configurations";
+    };
+
+    # New permissions option
+    permissions = mkOption {
+      type = permissionsType;
+      default = {};
+      description = "Permissions for created directories";
+    };
+
+    # New cleanup option
+    cleanup = {
+      enable = mkEnableOption "Clean up directories when disabled";
+      backup = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Create a backup before cleaning up directories";
+      };
     };
   };
 
   config = mkIf cfg.enable {
-    # Create the base directory structure
-    home.file = mkDirs;
+    # Create the base directory structure with permissions
+    home.file =
+      mkDirs
+      // {
+        ${cfg.baseDir} = {
+          source = null;
+          inherit (cfg.permissions) dirMode user group;
+        };
+      };
 
     # Add shell aliases
     programs.bash.shellAliases = {
